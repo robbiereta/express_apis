86ba0a35e4f9636d6083f4b4b21ceeba49491eca
Robbie Reta <robbie.reta67@gmail.com>
2024-04-25 11:36:33 -0600

    .

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -47,7 +47,15 @@ module.exports = {
    * productosController.create()
    */
   create: (req, res) => {
-    let productos = new productosModel({			precio : req.body.precio,			categoria : req.body.categoria,			descripcion : req.body.descripcion,			marca : req.body.marca,			especificaciones : req.body.especificaciones,			codigo : req.body.codigo,			moto : req.body.moto,			compatibilidades : req.body.compatibilidades
+    let productos = new productosModel({
+			precio : req.body.precio,
+			categoria : req.body.categoria,
+			descripcion : req.body.descripcion,
+			marca : req.body.marca,
+			especificaciones : req.body.especificaciones,
+			codigo : req.body.codigo,
+			moto : req.body.moto,
+			compatibilidades : req.body.compatibilidades
     });
 
     productos.save((err, productos) => {
@@ -79,7 +87,15 @@ module.exports = {
         });
       }
 
-      productos.precio = req.body.precio ? req.body.precio : productos.precio;			productos.categoria = req.body.categoria ? req.body.categoria : productos.categoria;			productos.descripcion = req.body.descripcion ? req.body.descripcion : productos.descripcion;			productos.marca = req.body.marca ? req.body.marca : productos.marca;			productos.especificaciones = req.body.especificaciones ? req.body.especificaciones : productos.especificaciones;			productos.codigo = req.body.codigo ? req.body.codigo : productos.codigo;			productos.moto = req.body.moto ? req.body.moto : productos.moto;			productos.compatibilidades = req.body.compatibilidades ? req.body.compatibilidades : productos.compatibilidades;			
+      productos.precio = req.body.precio ? req.body.precio : productos.precio;
+			productos.categoria = req.body.categoria ? req.body.categoria : productos.categoria;
+			productos.descripcion = req.body.descripcion ? req.body.descripcion : productos.descripcion;
+			productos.marca = req.body.marca ? req.body.marca : productos.marca;
+			productos.especificaciones = req.body.especificaciones ? req.body.especificaciones : productos.especificaciones;
+			productos.codigo = req.body.codigo ? req.body.codigo : productos.codigo;
+			productos.moto = req.body.moto ? req.body.moto : productos.moto;
+			productos.compatibilidades = req.body.compatibilidades ? req.body.compatibilidades : productos.compatibilidades;
+			
       productos.save( (err, productos) => {
         if (err) {
           return res.status(500).json({
