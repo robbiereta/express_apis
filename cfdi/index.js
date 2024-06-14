c789bffe8876c2b8873f6e238ed2e98898ccde27
Robbie Reta <robbie.reta67@gmail.com>
2024-06-13 21:32:34 -0600

    .

---
+++ cfdi/index.js
@@ -0,0 +1,47 @@
+//const key = 'CSD_Pruebas_CFDI_TCM970625MB1.key';
+//const cer = 'CSD_Pruebas_CFDI_TCM970625MB1.cer';
+import { 
+    CFDI, Emisor
+  } from '@cfdi/xml';
+  
+  const xslt = 'http://www.sat.gob.mx/sitio_internet/cfd/4/cadenaoriginal_4_0/cadenaoriginal_4_0.xslt';
+  
+  
+  const comprobanteAttribute = {
+      Serie: 'E',
+      Folio: 'ACACUN-27',
+      Fecha: '2014-07-08T12:16:50',
+
+  };
+  
+  const cfd = new CFDI(comprobanteAttribute, {
+      debug: true,
+      xslt: {
+        path: xslt,
+      },
+  });
+  cfd.setAttributesXml({ version: '1.0', encoding: 'utf-8' });
+  
+  cfd.informacionGlobal({
+      Periodicidad: '1',
+      Meses: '1',
+      Año: '2',
+  });
+  
+  const emisor = new Emisor({
+      Rfc: 'TCM970625MB1',
+      Nombre: 'FACTURACION MODERNA SA DE CV',
+      RegimenFiscal: 601,
+      FacAtrAdquirente: 'asdasd',
+  });
+  cfd.emisor(emisor);
+
+const key = './lib/k.key.pem';
+const cer = './lib/c.cer.pem';
+// await cfd.certificar(cer);
+// await cfd.sellar(key, '19032017');
+const xml = await cfd.getXmlCdfi();
+
+console.log(xml)
+
+export default xml
